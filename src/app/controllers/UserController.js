18fd185b12237355193b28b08e8630fcb2eada7e
Rafael Mello <rafaelmello0715@outlook.com>
2020-05-08 06:29:59 -0300

    feat: structuring a small server

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,17 @@
+class UserController {
+  async store(req, res) {
+    const { name, email, password } = req.body
+
+    const user = {
+      name,
+      email,
+      password,
+    }
+
+    // Adicionar job RegistrationMail na fila
+
+    return res.json(user)
+  }
+}
+
+export default new UserController()
